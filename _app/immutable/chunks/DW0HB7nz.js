2d4826d59b08425b98078cbb4577ee32bd7363e5
ishanjalan <ishanjalan@users.noreply.github.com>
2026-01-17 06:24:52 +0000

    Deploy from Neutron monorepo ishanjalan/Neutron@39312e2822026c9994d34e20fb8ab1fcfa78839d

--- _app/immutable/chunks/DW0HB7nz.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_ze3uri?.base??"/Squash",e=globalThis.__sveltekit_ze3uri?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/chunks/gkzu2dTj.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_ye6xi2?.base??"/Squash",e=globalThis.__sveltekit_ye6xi2?.assets??s??"";export{e as a,s as b};
